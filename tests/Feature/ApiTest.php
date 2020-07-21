afe67f4c07db883c918091144a97705c2b30a601
Bertrand Atemkeng <ba@marketing-factory.de>
2020-07-21 20:04:49 +0200

    add initial tests

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -13,9 +13,102 @@ use Tests\TestCase;
  */
 class ApiTest extends TestCase
 {
+    /**
+     * Verify if server is up
+     */
     public function testApiRoute()
     {
         $respose = $this->get('/');
         $respose->assertStatus(200);
+    }
+
+    /**
+     * Server should respond with some error message if query parameter is poorly formatted
+     */
+    public function testBadRequest()
+    {
+        $respose = $this->get('?q=%%Ae**m%');
+        $respose->assertStatus(400);
+    }
+
+    /**
+     * HInt user If query parameter is incomplete
+     */
+    public function testValidQueryParameter()
+    {
+        $respose = $this->get('?q');
+
+        $respose->assertSee('');
+    }
+
+    /**
+     * test against typo tolerance from TVMaze API
+     * eg deadwood should  return Deadwood
+     */
+    public function testSearchReturnsExactMatch()
+    {
+        $names =  [
+            'Deadwood',
+            'Prison Break',
+            'Outlander'
+        ];
+
+        foreach ($names as $name){
+
+            $respose = $this->get('?q='.$name);
+            $respose->assertSee($respose->original['Matching results for '.$name][0]->show->name);
+
+            self::assertSame($respose->original['Matching results for '.$name][0]->show->name, $name);
+        }
+    }
+
+    /**
+     * Tests if non matching search parameter returns suggestions
+     * eg prison should return adn aray of simmilar show names like   [Prison Break , ...]
+     */
+    public function testSearchNonExactMatchReturnSuggestion()
+    {
+        $names =  [
+            'Deadwoods',
+            'Prison Breakers',
+            'Trump'
+        ];
+
+        foreach ($names as $name){
+
+            $respose = $this->get('?q='.$name);
+            $respose->assertSee($respose->original['Matching results for '.$name][0]);
+
+            $jsonResponse = [
+                "Sorry we could not find any show named ".$name,
+                "Did you mean:",
+            ];
+
+            self::assertSame($respose->original['Matching results for '.$name][0], $jsonResponse[0]);
+            self::assertSame($respose->original['Matching results for '.$name][1], $jsonResponse[1]);
+        }
+    }
+
+    /**
+     * test against case sensitive  query parameter (tv shoe name) from TVMaze API
+     * eg deadwood should return Deadwood or deadwood
+     */
+    public function testSearchNonCaseSensitive()
+    {
+        $names =  [
+            'deadwood',
+            'prison Break',
+            'Outlander',
+            'qUEen'
+        ];
+
+        foreach ($names as $name){
+
+            $respose = $this->get('?q='.$name);
+            $respose->assertSee($respose->original['Matching results for '.$name][0]->show->name);
+
+            self::assertSame(strtolower($respose->original['Matching results for '.$name][0]->show->name),
+                strtolower($name));
+        }
     }
 }
